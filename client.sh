aa117e3736190195476fc31231286763715dd2c3
tjtanaa <tunjian.tan@akirakan.com>
2024-10-17 18:18:08 +0800

    fix directory in client script

--- client.sh
+++ client.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python vllm-vlm-bm/benchmarks/benchmark_serving.py \
+python /benchmarks/benchmark_serving.py \
     --backend "openai-chat" \
     --model  "/media/akk/hdd3/hf_model/Qwen/Qwen2-VL-7B-Instruct"\
     --dataset-name sharegpt \
